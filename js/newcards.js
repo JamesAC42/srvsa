dd41a2dc3eb4f77c39ca30e8c954ea1db54f4517
James Crovo <jamescrovo450@gmail.com>
2017-04-11 15:56:52 -0400

    .

--- js/newcards.js
+++ js/newcards.js
@@ -1,9 +1,12 @@
 jQuery(function($){
+
 	var TAB_KEY = 9;
 	var RIGHT_KEY = 39;
 	var LEFT_KEY = 37;
 	var ENTER_KEY = 13;
+
 	var CARD = "<div class='card added'><input type='text' tabindex='-1' maxlength='30'></div>";
+	
 	var util = {
 		store: function (namespace, data) {
 			if (arguments.length > 1) {
@@ -14,6 +17,7 @@ jQuery(function($){
 			}
 		}
 	}
+	
 	var App = {
 		init: function(){
 			this.cards = util.store('application-cards');
