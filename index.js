5cda40c56d78b418d27542d8a703b85d74c4b7d0
James Crovo <jamescrovo2019@lschs.org>
2017-04-23 15:37:06 -0400

    Fix Form processing

--- index.js
+++ index.js
@@ -2,196 +2,154 @@ var http = require("http");
 var fs = require("fs");
 var formidable = require("formidable");
 var path = require("path");
-var async = require("async");
 var parseString = require('xml2js').parseString;
 
 var server = http.createServer(function(req, res){
-	if (req.method.toLowerCase() == 'get'){
-		displayForm(req, res);
-	} else if (req.method.toLowerCase() == 'post'){
-		processForm(req, res);
-	}
+  if (req.method.toLowerCase() == 'get'){
+    displayForm(req, res);
+  } else if (req.method.toLowerCase() == 'post'){
+    processForm(req, res);
+  }
 });
 
 function displayForm(req, res){
-	var filePath = req.url;
-	if (filePath == '/')
-	  filePath = '/newcards.html';
-	filePath = __dirname+filePath;
-	var extname = path.extname(filePath);
-	var contentType = 'text/html';
-	switch (extname) {
-	    case '.js':
-	        contentType = 'text/javascript';
-	        break;
-	    case '.css':
-	        contentType = 'text/css';
-	        break;
-	}
-	fs.exists(filePath, function(exists) {
-	    if (exists) {
-	        fs.readFile(filePath, function(error, content) {
-	            if (error) {
-	                res.writeHead(500);
-	                res.end();
-	            }
-	            else {                   
-	                res.writeHead(200, { 'Content-Type': contentType });
-	                res.end(content, 'utf-8');                  
-	            }
-	        });
-	    }
-	});
+  var filePath = req.url;
+  if (filePath == '/')
+    filePath = '/newcards.html';
+  filePath = __dirname+filePath;
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+      case '.js':
+          contentType = 'text/javascript';
+          break;
+      case '.css':
+          contentType = 'text/css';
+          break;
+  }
+  fs.exists(filePath, function(exists) {
+      if (exists) {
+          fs.readFile(filePath, function(error, content) {
+              if (error) {
+                  res.writeHead(500);
+                  res.end();
+              }
+              else {                   
+                  res.writeHead(200, { 'Content-Type': contentType });
+                  res.end(content, 'utf-8');                  
+              }
+          });
+      }
+  });
 }
 
 function processForm(req, res){
-	console.log("recieved");
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files){
-		res.writeHead(200, {
-			'content-type': 'text/plain'
-		});
-		var words = JSON.parse(fields.words);
-		words = words.filter(filterWords);
-		var worddefs = [];
-		async.each(words,
-			function(word, callback){
-				console.log("wording");
-				var sanitizedw = word.replace(/\s/g, '');
-				sanitizedw = sanitizedw.replace(/[^A-Za-z\s!?]/g,'');
-				if(!sanitizedw === ''){
-					var wpath = '/api/v1/references/collegiate/xml/' + sanitizedw + '?key=ef9d9b7a-b5fd-493f-8962-d8b89d1e4ba8';
-					var options = {
-					    host: 'www.dictionaryapi.com',
-					    path: wpath
-					};
-					http.get(options, function (http_res) {
-					    var data = "";
-					    http_res.on("data", function (chunk) {
-					        data += chunk;
-					    });
-					    http_res.on("end", function () {
-					    	console.log('hello there');
-					    	parseString(data, function(err, result){
-					    		if("suggestion" in result.entry_list){
-					    			var entry = {
-					    				word: word,
-					    				mainDefinition: '',
-					    				definitions: [],
-					    				hasDefinitions: false
-					    			}
-					    		}else{
-					    			var definitions = result.entry_list.entry[0].def[0].dt;
-					    			definitions = definitions.filter(filterDefs);
-					    			var mainDefinition = definitions[0];
-					    			definitions.splice(0,1);
-					    			var entry = {
-					    				word:word,
-					    				mainDefinition: mainDefinition,
-					    				definitions: definitions,
-					    				hasDefinitions: true
-					    			};
-					    		}
-					    	});
-					    	worddefs.push(entry);
-					    	callback();
-					    });
-					});
-				}
-			},
-			function(err){
-				console.log('hello');
-				if(!err){
-					var title = JSON.parse(fields.title)
-					title.replace(/[^A-Za-z\s!?]/g,'');
-					var dataJson = {
-						title: title,
-						words: worddefs
-					};
-					var filename = Math.random().toString(36).substring(2, 7);
-					var filepath = "./data/" + filename + ".json";
-					fs.writeFile(filepath, JSON.stringify(dataJson), "utf8");
-					console.log(dataJson);
-					res.write('http://localhost:3000/');
-					res.end();
-				}
-			}
-		);
-	});
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files){
+    res.writeHead(200, {
+      'content-type': 'text/plain'
+    });
+    var words = JSON.parse(fields.words);
+    words = words.filter(filterWords);
+    Promise.all(words.map(sanitizedw => new Promise ((done, reject) => {
+      sanitizedw = sanitizedw.replace(/[^a-zA-Z/s]/g,'').trim();
+      var encodedw = encodeURIComponent(sanitizedw);
+      var wpath = '/api/v1/references/collegiate/xml/' + encodedw + '?key=ef9d9b7a-b5fd-493f-8962-d8b89d1e4ba8';
+      var options = {
+          host: 'www.dictionaryapi.com',
+          path: wpath
+      };
+      http.get(options, function (http_res) {
+        var data = "";
+        http_res.on("data", function (chunk) {
+            data += chunk;
+        });
+        http_res.on("end", function () {
+          parseString(data, function(err, result) {
+            console.info(JSON.stringify(result), null, '  ');
+            let entry;
+            if (err) {
+              reject(err);
+              return;
+            }
+            const entryList = result['entry_list'];
+            if (!entryList.entry) {
+              if (entryList.suggestion) {
+                const suggestion = entryList.suggestion;
+                entry = {
+                  sanitizedw,
+                  mainDefinition: '',
+                  definitions: [],
+                  hasDefinitions: false,
+                  suggestions: Array.isArray(suggestion) ? suggestion : [ suggestion ]
+                }
+              } else {
+                entry = {
+                  sanitizedw,
+                  mainDefinition: '',
+                  definitions: [],
+                  hasDefinitions: false,
+                  suggestions: []
+                }
+              }
+            } else {
+              var definitionsSpagget = entryList.entry[0].def[0].dt;
+              var defs = definitionsSpagget.map(function(def){
+                let cleanDef;
+                if (typeof def === 'object') {
+                  cleanDef = def["_"].slice(1).trim();
+                } else if (typeof def === 'string') {
+                  cleanDef = def.slice(1).trim();
+                }
+                if (cleanDef !== '' || typeof cleanDef == 'object'){
+                  return cleanDef;
+                } else {
+                  return;
+                }
+              })
+              var mainDefinition = defs[0];
+              defs.splice(0,1);
+              entry = {
+                sanitizedw,
+                mainDefinition: mainDefinition,
+                definitions: defs,
+                hasDefinitions: true,
+                suggestions: []
+              };
+            }
+            done(entry);
+          }); 
+        });
+      });
+    }))).then(results => {
+      console.info(results);
+      var title = JSON.parse(fields.title)
+      title.replace(/[^A-Za-z\s0-9!?]/g,'');
+      var dataJson = {
+        title,
+        words: results
+      };
+      var filename = Math.random().toString(36).substring(2, 7);
+      var filepath = "./data/sets/" + filename + ".json";
+      var dataset = require("./data/sets.json");
+      dataset["files"].push(filename);
+      fs.writeFile("./data/sets.json", JSON.stringify(dataset), "utf8");
+      fs.writeFile(filepath, JSON.stringify(dataJson, null, '  '), "utf8");
+      console.info(dataJson);
+      res.write('http://localhost:3000/');
+      res.end();
+    }).catch(err => {
+      console.error(err);
+    });
+  });
 }
 
 function filterDefs(input){
-	return typeof input === "string";
+  return typeof input === "string";
 }
 
 function filterWords(input){
-	return input !== '';
-}
-
-function defineWord(word){
-	var wpath = '/api/v1/references/collegiate/xml/' + word + '?key=ef9d9b7a-b5fd-493f-8962-d8b89d1e4ba8';
-	var options = {
-	    host: 'www.dictionaryapi.com',
-	    path: wpath
-	};
-	http.get(options, function (http_res) {
-	    var data = "";
-	    http_res.on("data", function (chunk) {
-	        data += chunk;
-	    });
-	    http_res.on("end", function () {
-	    	parseString(data, function(err, result){
-	    		if("suggestion" in result.entry_list){
-	    			var entry = {
-	    				word: word,
-	    				mainDefinition: '',
-	    				definitions: [],
-	    				hasDefinitions: false
-	    			}
-	    		}else{
-	    			var definitions = result.entry_list.entry[0].def[0].dt;
-	    			definitions = definitions.filter(filterDefs);
-	    			var mainDefinition = definitions[0];
-	    			definitions.splice(0,1);
-	    			var entry = {
-	    				word:word,
-	    				mainDefinition: mainDefinition,
-	    				definitions: definitions,
-	    				hasDefinitions: true
-	    			};
-	    		}
-	    		return entry;
-	    	});
-	    });
-	});
-}
-
-function asyncLoop(iterations, func, callback) {
-    var index = 0;
-    var done = false;
-    var loop = {
-        next: function() {
-            if (done) {
-                return;
-            }
-            if (index < iterations) {
-                index++;
-                func(loop);
-            } else {
-                done = true;
-                callback();
-            }
-        },
-        iteration: function() {
-            return index - 1;
-        },
-        break: function() {
-            done = true;
-            callback();
-        }
-    };
-    loop.next();
-    return loop;
+  return input !== '';
 }
 
 server.listen(3000);
--- js/newcards.js
+++ js/newcards.js
@@ -1,239 +1,252 @@
 jQuery(function($){
 
-	var TAB_KEY = 9;
-	var RIGHT_KEY = 39;
-	var LEFT_KEY = 37;
-	var ENTER_KEY = 13;
+    var TAB_KEY = 9;
+    var RIGHT_KEY = 39;
+    var LEFT_KEY = 37;
+    var ENTER_KEY = 13;
 
-	var CARD = "<div class='card added'><input type='text' tabindex='-1' maxlength='30'></div>";
-	
-	var util = {
-		store: function (namespace, data) {
-			if (arguments.length > 1) {
-				return localStorage.setItem(namespace, JSON.stringify(data));
-			} else {
-				var store = localStorage.getItem(namespace);
-				return (store && JSON.parse(store)) || [''];
-			}
-		}
-	}
-	
-	var App = {
-		init: function(){
-			this.cards = util.store('application-cards');
-			this.cardamt = 1;
-			this.focus = 1;
-			this.title = '';
-			this.bindEvents();
-			this.render();
-		},
-		focusIsLast:function(){
-			return this.focus == this.cardamt;
-		},
-		bindEvents: function(){
-			$('#next').on('click', this.nextCard.bind(this));
-			$('#back').on('click', this.prevCard.bind(this));
-			$('#add').on('click', this.addCard.bind(this));
-			$('#remove').on('click', this.removeCard.bind(this));
-			$('#create').on('click', this.submitSet.bind(this));
-			$('#ff-next').on('click', this.gotoLast.bind(this));
-			$('#ff-back').on('click', this.gotoFirst.bind(this));
-			$('#hide-error').on('click', this.hideModal.bind(this));
-			$('form')
-				.on('keydown', 'input', this.cardLog.bind(this))
-				.on('keyup', 'input', this.wordLog.bind(this));
-			$('div.title-container input').on('keydown', this.setTitle.bind(this));
-			$('body').on('keydown', this.cardMove.bind(this));
-		},
-		disable: function(el){
-			$(el).addClass('disabled');
-		},
-		enable: function(el){
-			$(el).removeClass('disabled');
-		},
-		addCard: function(e){
-			var focus = this.focus;
-			var cards = this.cards;
-			$('div.card:nth-child(' + focus + ')')
-				.addClass('left-card')
-				.after(CARD);
-			cards.splice(focus, 0, '');
-			this.cardamt++;
-			focus++;
-			this.cards = cards;
-			this.focus = focus;
-			this.render();
-		},
-		removeCard: function(){
-			var focus = this.focus;
-			var cards = this.cards;
-			if(!(this.cardamt > 1)){
-				return;
-			} else {
-				$('div.card:nth-child(' + focus + ')').remove();
-				cards.splice((focus - 1), 1);
-				if(this.focusIsLast()){
-					focus--;
-					$('div.card:nth-child(' + focus + ')').removeClass('left-card');
-				} else {
-					$('div.card:nth-child(' + focus + ')').removeClass('right-card');
-				}
-				this.cards = cards;
-				this.focus = focus;
-				this.cardamt--;
-				this.render();
-			}
-		},
-		nextCard: function(){
-			var focus = this.focus;
-			if(!this.focusIsLast()){
-				$('div.card:nth-child(' + focus + ')').blur().addClass('left-card');
-				focus++;
-				$('div.card:nth-child(' + focus + ')').removeClass('right-card');
-				this.focus = focus;
-				this.render();
-			}
-		},
-		prevCard: function(){
-			var focus = this.focus;
-			if(!(focus == 1)){
-				$('div.card:nth-child(' + focus + ')').addClass('right-card');
-				focus--;
-				$('div.card:nth-child(' + focus + ')').removeClass('left-card');
-				this.focus = focus;
-				this.render();
-			}
-		},
-		gotoFirst: function(){
-			while(this.focus !== 1){
-				this.prevCard();
-			}
-		},
-		gotoLast: function(){
-			while(!this.focusIsLast()){
-				this.nextCard();
-			}
-		},
-		cardLog: function(e){
-			if(e.which == TAB_KEY){
-				e.preventDefault();
-				if(e.shiftKey){
-					this.prevCard();
-				} else {
-					if(this.focusIsLast()){
-						this.addCard();
-					} else {
-						$('div.card:nth-child(' + this.focus + ')').blur();
-						this.nextCard();
-						return false;
-					}
-				}
-				$('div.card:nth-child(' + this.focus + ') input').focus();
-			}else if(e.which == ENTER_KEY){
-				e.preventDefault();
-				this.addCard();
-				$('div.card:nth-child(' + this.focus + ') input').focus();
-			} else {
-				return;
-			}
-		},
-		wordLog: function(e){
-			var cards = this.cards;
-			var focus = this.focus;
-			if(e.which == TAB_KEY || e.which == ENTER_KEY){
-				return;
-			} else {
-				var word = $(e.target).val().trim();
-				cards[focus-1] = word;
-				this.cards = cards;
-				this.render();
-			}
-		},
-		cardMove: function(e){
-			var focus = this.focus;
-			if(!$('div.card input').is(':focus') && !$('div.title-container input').is(':focus')){
-				if(e.which !== ENTER_KEY && e.which !== RIGHT_KEY && e.which !== LEFT_KEY){
-					return false;
-				} else {
-					if(e.which == ENTER_KEY){
-						if(e.shiftKey){
-							this.removeCard();
-						} else {
-							this.addCard();
-						}
-					}else if(e.which == RIGHT_KEY){
-						this.nextCard();
-					}else if(e.which = LEFT_KEY){
-						this.prevCard();
-					} else {
-						return false;
-					}
-				}
+    var CARD = "<div class='card added'><input type='text' tabindex='-1' maxlength='30'></div>";
+    
+    var util = {
+        store: function (namespace, data) {
+            if (arguments.length > 1) {
+                return localStorage.setItem(namespace, JSON.stringify(data));
+            } else {
+                var store = localStorage.getItem(namespace);
+                return (store && JSON.parse(store)) || [''];
+            }
+        }
+    }
+    
+    var App = {
+        init: function(){
+            this.cards = util.store('application-cards');
+            this.cardamt = 1;
+            this.focus = 1;
+            this.title = '';
+            this.bindEvents();
+            this.render();
+        },
+        focusIsLast:function(){
+            return this.focus == this.cardamt;
+        },
+        bindEvents: function(){
+            $('#next').on('click', this.nextCard.bind(this));
+            $('#back').on('click', this.prevCard.bind(this));
+            $('#add').on('click', this.addCard.bind(this));
+            $('#remove').on('click', this.removeCard.bind(this));
+            $('#create').on('click', this.submitSet.bind(this));
+            $('#ff-next').on('click', this.gotoLast.bind(this));
+            $('#ff-back').on('click', this.gotoFirst.bind(this));
+            $('#hide-error').on('click', this.hideModal.bind(this));
+            $('form')
+                .on('keydown', 'input', this.cardLog.bind(this))
+                .on('keyup', 'input', this.wordLog.bind(this));
+            $('div.title-container input').on('keydown', this.setTitle.bind(this));
+            $('body').on('keydown', this.cardMove.bind(this));
+        },
+        disable: function(el){
+            $(el).addClass('disabled');
+        },
+        enable: function(el){
+            $(el).removeClass('disabled');
+        },
+        addCard: function(e){
+            var focus = this.focus;
+            var cards = this.cards;
+            $('div.card:nth-child(' + focus + ')')
+                .addClass('left-card')
+                .after(CARD);
+            cards.splice(focus, 0, '');
+            this.cardamt++;
+            focus++;
+            this.cards = cards;
+            this.focus = focus;
+            this.render();
+        },
+        removeCard: function(){
+            var focus = this.focus;
+            var cards = this.cards;
+            if(!(this.cardamt > 1)){
+                return;
+            } else {
+                $('div.card:nth-child(' + focus + ')').remove();
+                cards.splice((focus - 1), 1);
+                if(this.focusIsLast()){
+                    focus--;
+                    $('div.card:nth-child(' + focus + ')').removeClass('left-card');
+                } else {
+                    $('div.card:nth-child(' + focus + ')').removeClass('right-card');
+                }
+                this.cards = cards;
+                this.focus = focus;
+                this.cardamt--;
+                this.render();
+            }
+        },
+        nextCard: function(){
+            var focus = this.focus;
+            if(!this.focusIsLast()){
+                $('div.card:nth-child(' + focus + ')').blur().addClass('left-card');
+                focus++;
+                $('div.card:nth-child(' + focus + ')').removeClass('right-card');
+                this.focus = focus;
+                this.render();
+            }
+        },
+        prevCard: function(){
+            var focus = this.focus;
+            if(!(focus == 1)){
+                $('div.card:nth-child(' + focus + ')').addClass('right-card');
+                focus--;
+                $('div.card:nth-child(' + focus + ')').removeClass('left-card');
+                this.focus = focus;
+                this.render();
+            }
+        },
+        gotoFirst: function(){
+            while(this.focus !== 1){
+                this.prevCard();
+            }
+        },
+        gotoLast: function(){
+            while(!this.focusIsLast()){
+                this.nextCard();
+            }
+        },
+        cardLog: function(e){
+            if(e.which == TAB_KEY){
+                e.preventDefault();
+                if(e.shiftKey){
+                    this.prevCard();
+                } else {
+                    if(this.focusIsLast()){
+                        this.addCard();
+                    } else {
+                        $('div.card:nth-child(' + this.focus + ')').blur();
+                        this.nextCard();
+                        return false;
+                    }
+                }
+                $('div.card:nth-child(' + this.focus + ') input').focus();
+            }else if(e.which == ENTER_KEY){
+                e.preventDefault();
+                this.addCard();
+                $('div.card:nth-child(' + this.focus + ') input').focus();
+            } else {
+                return;
+            }
+        },
+        wordLog: function(e){
+            var cards = this.cards;
+            var focus = this.focus;
+            if(e.which == TAB_KEY || e.which == ENTER_KEY){
+                return;
+            } else {
+                var word = $(e.target).val().trim();
+                cards[focus-1] = word;
+                this.cards = cards;
+                this.render();
+            }
+        },
+        cardMove: function(e){
+            var focus = this.focus;
+            if(!$('div.card input').is(':focus') && !$('div.title-container input').is(':focus')){
+                if(e.which !== ENTER_KEY && e.which !== RIGHT_KEY && e.which !== LEFT_KEY){
+                    return false;
+                } else {
+                    if(e.which == ENTER_KEY){
+                        if(e.shiftKey){
+                            this.removeCard();
+                        } else {
+                            this.addCard();
+                        }
+                    }else if(e.which == RIGHT_KEY){
+                        this.nextCard();
+                    }else if(e.which = LEFT_KEY){
+                        this.prevCard();
+                    } else {
+                        return false;
+                    }
+                }
 
-			} else {
-				return;
-			}
-		},
-		setTitle: function(){
-			this.title = $('div.title-container input').val().trim();
-			this.render();
-		},
-		canSubmit: function(){
-			var validamt = 0;
-			$('div.card').each(function(){
-				var val = $(this).children('input').val().trim();
-				if(val !== ''){
-					validamt++;
-				}
-			});
-			var submitbool = (validamt >= 5) && (this.title !== '');
-			return(submitbool);
-		},
-		hideModal: function(){
-			$('div.error').removeClass('modal-visible');
-		},
-		submitSet: function(){
-			if(this.canSubmit()){
-				$.post("/", {title: JSON.stringify(this.title), words: JSON.stringify(this.cards)}, data=>document.location.href = data);
-			} else {
-				$('div.error').addClass('modal-visible');
-			}
-		},
-		render: function(){
-			var progress = Math.round((this.focus / this.cardamt) * 100);
-			$('.progress-bar-inner').css('width', progress + '%');
-			if(progress == 100){
-				$('.progress-bar-inner').addClass('progress-bar-complete');
-			} else {
-				$('.progress-bar-inner').removeClass('progress-bar-complete');
-			}
-			if(this.focusIsLast()){
-				this.disable($('#next'));
-				if(this.focus == 1){
-					this.disable($('#back'));
-				} else {
-					this.enable($('#back'));
-				}
-			} else {
-				this.enable($('#next'));
-				if(this.focus == 1){
-					this.disable($('#back'));
-				} else {
-					this.enable($('#back'));
-				}
-			}
-			if(this.cardamt == 1){
-				this.disable($('#remove'));
-			} else {
-				this.enable($('#remove'));
-			}
-			$('#card-amt').text(this.focus + ' / ' + this.cardamt);
-			if(this.canSubmit()){
-				this.enable($('#create'));
-			} else {
-				this.disable($('#create'));
-			}
-		}
-	}	
-	App.init();
+            } else {
+                return;
+            }
+        },
+        setTitle: function(e){
+            if(e.which == TAB_KEY){
+                e.preventDefault();
+                $('div.title-container input').blur();
+                $('div.card:nth-child(' + this.focus + ') input').focus();
+            }else{
+                this.title = $('div.title-container input').val().trim();
+                this.render();
+                return;
+            }
+        },
+        canSubmit: function(){
+            var validamt = 0;
+            $('div.card').each(function(){
+                var val = $(this).children('input').val().trim();
+                if(val !== ''){
+                    validamt++;
+                }
+            });
+            var submitbool = (validamt >= 5) && (this.title !== '');
+            return(submitbool);
+        },
+        hideModal: function(){
+            $('div.error').removeClass('modal-visible');
+        },
+        submitSet: function(){
+            this.title = $('div.title-container input').val().trim();
+            if(this.canSubmit()){
+                $.post("/", {title: JSON.stringify(this.title), words: JSON.stringify(this.cards)}, data=>document.location.href = data);
+            } else {
+                if (!this.title == ''){
+                    $('div.message').text("You need at least 5 valid cards to create a set.")
+                } else {
+                    $('div.message').text("You need a title to create a set.");
+                }
+                $('div.error').addClass('modal-visible');
+            }
+        },
+        render: function(){
+            var progress = Math.round((this.focus / this.cardamt) * 100);
+            $('.progress-bar-inner').css('width', progress + '%');
+            if(progress == 100){
+                $('.progress-bar-inner').addClass('progress-bar-complete');
+            } else {
+                $('.progress-bar-inner').removeClass('progress-bar-complete');
+            }
+            if(this.focusIsLast()){
+                this.disable($('#next'));
+                if(this.focus == 1){
+                    this.disable($('#back'));
+                } else {
+                    this.enable($('#back'));
+                }
+            } else {
+                this.enable($('#next'));
+                if(this.focus == 1){
+                    this.disable($('#back'));
+                } else {
+                    this.enable($('#back'));
+                }
+            }
+            if(this.cardamt == 1){
+                this.disable($('#remove'));
+            } else {
+                this.enable($('#remove'));
+            }
+            $('#card-amt').text(this.focus + ' / ' + this.cardamt);
+            if(this.canSubmit()){
+                this.enable($('#create'));
+            } else {
+                this.disable($('#create'));
+            }
+        }
+    }   
+    App.init();
 })
